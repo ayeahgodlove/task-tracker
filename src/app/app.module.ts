17c1886f530a67d620474e2467bc68eb0bf3a615
ayeahgodlove <ayeahgodlove5@gmail.com>
2022-07-01 23:01:34 +0100

    feat(button): Create dynamic button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { ButtonComponent } from './components/button/button.component';
+
+/**
+ * Angular Componet modules
+ */
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    
-  ],
+  declarations: [AppComponent, HeaderComponent, ButtonComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
